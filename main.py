38c32e788b1163f9f686698a42d7b0e97bcd5a6e
isaacophoven <824067@mystma.org>
2021-09-28 09:31:06 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #WRITE YOUR CODE IN THIS FILE
+#ok
+
+def close10(x,y):
+    if x - 10 < y - 10:
+        return x
+    elif x - 10 > y - 10:
+        return y
+    else:
+        return 0
+        
+print(close10(12,9))
